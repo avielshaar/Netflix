5c530ec5f3536c17c1dd7eb2403ddb105881cbb1
Aviel Shaar <shaaraviel@gmail.com>
2024-02-13 13:26:35 +0200

    update seedRouted for new object List

--- backend/routes/seedRouter.js
+++ backend/routes/seedRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import seedData from "../controllers/seedControllers.js";
 import expressAsyncHandler from "express-async-handler";
 
-const seedRouter=express.Router();
-seedRouter.get('/',expressAsyncHandler(seedData));
+const seedRouter = express.Router();
+seedRouter.get("/", expressAsyncHandler(seedData));
 
 export default seedRouter;
