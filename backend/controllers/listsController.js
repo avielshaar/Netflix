1ffb69458a067d6c962b4b813c0bb0fb6056b5b2
Aviel Shaar <shaaraviel@gmail.com>
2024-02-13 13:27:27 +0200

    Delete get by id and token

--- backend/controllers/listsController.js
+++ backend/controllers/listsController.js
@@ -5,34 +5,12 @@ const getMovieLists = async (req, res) => {
   res.send(lists);
 };
 
-const getSerieLists = async (req, res) => {
+const getSeriesLists = async (req, res) => {
     const lists = await List.find({ isSeries: true });
     res.send(lists);
   };
 
-const getListById = async (req, res) => {
-  const list = await List.findById(req.params.id);
-
-  if (list) {
-    res.send(list);
-  } else {
-    res.status(400).send({ message: "List not found" });
-  }
-};
-
-const getListByToken = async (req, res) => {
-  const { token } = req.params;
-  const list = await List.findOne({ token: token });
-  if (list) {
-    res.send(list);
-  } else {
-    res.status(400).send({ message: "List not found" });
-  }
-};
-
 export {
   getMovieLists,
-  getSerieLists,
-  getListById,
-  getListByToken
+  getSeriesLists
 };
