1ff9914235cdc64379719fb4709815183e77a358
Aviel Shaar <shaaraviel@gmail.com>
2024-03-14 13:25:47 +0200

    tring to fix vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import { UserProvider } from './contexts/UserContext.jsx';
 import { ContentProvider } from './contexts/ContentContext.jsx';
 
-axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://netflix-backend-theta.vercel.app/';
+axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://netflix-backend-nine.vercel.app';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserProvider>
