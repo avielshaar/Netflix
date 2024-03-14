a1d0de6eb75717733d11e847d7b36c67512dd466
Aviel Shaar <shaaraviel@gmail.com>
2024-03-14 13:07:26 +0200

    some changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,16 +8,14 @@ import { UserProvider } from './contexts/UserContext.jsx';
 import { ContentProvider } from './contexts/ContentContext.jsx';
 
 // http://localhost:8080
-axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://netflix-backend-three.vercel.app';
+axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://netflix-pied-pi.vercel.app/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <UserProvider>
-      <ContentProvider>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </ContentProvider>
-    </UserProvider>
-  
+  <UserProvider>
+    <ContentProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </ContentProvider>
+  </UserProvider>
 );
